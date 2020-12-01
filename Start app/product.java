2482b44b53a13fe533613ce1b604e59580484598
ahmad muzakky <69641708+Ahmad-Muzakky@users.noreply.github.com>
2020-12-02 00:34:14 +0700

    update product.java versi 2

--- Start app/product.java
+++ Start app/product.java
@@ -2,10 +2,17 @@ package com.example.mobileprogproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class product extends AppCompatActivity {
 
+    private Button btnMinus1,btnAdd1,btnMinus2,btnAdd2,btnMinus3,btnAdd3,btnPesanan,btnMinus4,btnAdd4;
+    private TextView tvItem1,tvJumlah1,tvItem2,tvJumlah2,tvItem3,tvJumlah3,tvItem4,tvJumlah4;
     model shared;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +22,144 @@ public class product extends AppCompatActivity {
         shared = (model) this.getApplication();
 
         addproduct();
+        btnPesanan = findViewById(R.id.btnPesanan);
+        btnMinus1 = findViewById(R.id.btnMinus1);
+        btnAdd1 = findViewById(R.id.btnAdd1);
+        btnMinus2 = findViewById(R.id.btnMinus2);
+        btnAdd2  = findViewById(R.id.btnAdd2);
+        btnMinus3 = findViewById(R.id.btnMinus3);
+        btnAdd3  = findViewById(R.id.btnAdd3);
+        btnMinus4 = findViewById(R.id.btnMinus4);
+        btnAdd4  = findViewById(R.id.btnAdd4);
+        tvItem1 = findViewById(R.id.tvItem1);
+        tvJumlah1 = findViewById(R.id.tvJumlah1);
+        tvItem2 = findViewById(R.id.tvItem2);
+        tvJumlah2 = findViewById(R.id.tvJumlah2);
+        tvItem3 = findViewById(R.id.tvItem3);
+        tvJumlah3 = findViewById(R.id.tvJumlah3);
+        tvItem4 = findViewById(R.id.tvItem4);
+        tvJumlah4 = findViewById(R.id.tvJumlah4);
+
+        btnMinus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == btnMinus1){
+
+                    Integer vjumlah = shared.getJumlah1();
+                    if(vjumlah>0){
+                        vjumlah = vjumlah - 1;
+                        shared.setJumlah1(vjumlah);
+                    }
+                    tvJumlah1.setText(vjumlah.toString());
+                }
+            }
+        });
+        btnMinus2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == btnMinus2){
+
+                    Integer vjumlah = shared.getJumlah2();
+                    if(vjumlah>0){
+                        vjumlah = vjumlah - 1;
+                        shared.setJumlah2(vjumlah);
+                    }
+                    tvJumlah2.setText(vjumlah.toString());
+                }
+            }
+        });
+        btnMinus3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == btnMinus3){
+
+                    Integer vjumlah = shared.getJumlah3();
+                    if(vjumlah>0){
+                        vjumlah = vjumlah - 1;
+                        shared.setJumlah3(vjumlah);
+                    }
+                    tvJumlah3.setText(vjumlah.toString());
+                }
+            }
+        });
+        btnMinus4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == btnMinus4){
+
+                    Integer vjumlah = shared.getJumlah4();
+                    if(vjumlah>0){
+                        vjumlah = vjumlah - 1;
+                        shared.setJumlah4(vjumlah);
+                    }
+                    tvJumlah4.setText(vjumlah.toString());
+                }
+            }
+        });
+        btnAdd1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == btnAdd1){
+
+                    Integer vjumlah = shared.getJumlah1();
+
+                    vjumlah = vjumlah + 1;
+                    shared.setJumlah1(vjumlah);
+
+                    tvJumlah1.setText(vjumlah.toString());
+                }
+            }
+        });
+        btnAdd2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == btnAdd2){
+
+                    Integer vjumlah = shared.getJumlah2();
+
+                    vjumlah = vjumlah + 1;
+                    shared.setJumlah2(vjumlah);
+
+                    tvJumlah2.setText(vjumlah.toString());
+                }
+            }
+        });
+        btnAdd3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == btnAdd3){
+
+                    Integer vjumlah = shared.getJumlah3();
+
+                    vjumlah = vjumlah + 1;
+                    shared.setJumlah3(vjumlah);
+
+                    tvJumlah3.setText(vjumlah.toString());
+                }
+            }
+        });
+        btnAdd4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == btnAdd4){
+
+                    Integer vjumlah = shared.getJumlah4();
+
+                    vjumlah = vjumlah + 1;
+                    shared.setJumlah4(vjumlah);
+
+                    tvJumlah4.setText(vjumlah.toString());
+                }
+            }
+        });
+        btnPesanan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == btnPesanan){
+                    startActivity(new Intent(product.this, order.class));
+                }
+            }
+        });
     }
 
     public void addproduct(){
