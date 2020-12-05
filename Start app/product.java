e2b3ba5f889d7e1931c5833f93c20d82583d97db
ahmad muzakky <69641708+Ahmad-Muzakky@users.noreply.github.com>
2020-12-05 17:26:53 +0700

    penambahan bottom navbar

--- Start app/product.java
+++ Start app/product.java
@@ -1,15 +1,19 @@
 package com.example.mobileprogproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class product extends AppCompatActivity {
 
@@ -24,12 +28,32 @@ public class product extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        BottomNavigationView navbar = findViewById(R.id.bottom_navigation);
+        navbar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),home.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.product:
+                        return true;
+                    case R.id.chart:
+                        startActivity(new Intent(getApplicationContext(),order.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
         orderbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openorder();
             }
         });
+
 
         model[] list = new model[]{
                 new model("Iphone",R.drawable.iphone,3000000),
