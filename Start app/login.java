e7c905d670e87839c4ef8a44459873a43af2965e
ahmad muzakky <69641708+Ahmad-Muzakky@users.noreply.github.com>
2020-12-05 15:58:02 +0700

    update login

--- Start app/login.java
+++ Start app/login.java
@@ -7,29 +7,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
     private Button log;
-    private EditText user,pass;
-
+    private TextView sign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        user = (EditText) findViewById(R.id.username);
-        pass = (EditText) findViewById(R.id.password);
         log = (Button) findViewById(R.id.loginbutt);
+        sign = (TextView) findViewById(R.id.sign);
         log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 nextpage();
             }
         });
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextpage1();
+            }
+        });
     }
     public void nextpage(){
         Intent intent = new Intent(this,home.class);
+        startActivity(intent);
+    }
+    public void nextpage1(){
+        Intent intent = new Intent(this,register.class);
         startActivity(intent);
     }
 }
